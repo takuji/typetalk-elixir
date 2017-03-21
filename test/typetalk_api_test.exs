c36af8b67b5099819abb98a09c6f1ec7523523e9
Takuji Shimokawa <takuji.shimokawa@gmail.com>
2017-03-21 22:57:50 +0900

    Use the environment variable

--- test/typetalk_api_test.exs
+++ test/typetalk_api_test.exs
@@ -2,16 +2,18 @@ defmodule TypeTalkTest do
   use ExUnit.Case
   doctest TypeTalk
 
+  @client_id System.get_env("TYPETALK_CLIENT_ID")
+  @client_secret System.get_env("TYPETALK_CLIENT_SECRET")
+
   test "the truth" do
     assert 1 + 1 == 2
   end
 
   test "access_token" do
     {:ok, json} = TypeTalk.access_token(
-      client_id: "your client id",
-      client_secret: "your client secret",
+      client_id: @client_id,
+      client_secret: @client_secret
     )
-    IO.inspect json
     assert json["access_token"] != nil
   end
 end
