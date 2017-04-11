c320fa7ecdf34403e024cf6fba81d42551c9b255
Takuji Shimokawa <takuji.shimokawa@gmail.com>
2017-04-12 02:07:14 +0900

    Add a primitive gen_server.

---
+++ lib/typetalk/server.ex
@@ -0,0 +1,26 @@
+defmodule Typetalk.Server do
+  @moduledoc """
+  """
+  use GenServer
+
+  #
+  # Client APIs
+  #
+
+  def start_link(auth) do
+    GenServer.start_link(__MODULE__, auth)
+  end
+
+  def call(pid, api, args \\ []) do
+    GenServer.call(pid, {api, args})
+  end
+
+  #
+  # OTP callbacks
+  #
+
+  defp handle_call({api, args}, _from, auth) do
+    result = apply(Typetalk, api, [auth | args])
+    {:reply, result, auth}
+  end
+end
