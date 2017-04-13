3bded21a1fd03e4122427af6447493667200fa3c
Takuji Shimokawa <takuji.shimokawa@gmail.com>
2017-04-13 23:53:27 +0900

    Add package information.

--- mix.exs
+++ mix.exs
@@ -1,12 +1,19 @@
 defmodule Typetalk.Mixfile do
   use Mix.Project
 
+  @description """
+  Typetalk API client for Elixir
+  """
+
   def project do
     [app: :typetalk,
      version: "0.1.0",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     name: "Typetalk",
+     description: @description,
+     package: package(),
      deps: deps()]
   end
 
@@ -34,6 +41,14 @@ defmodule Typetalk.Mixfile do
       {:poison, "~> 3.1"},
       {:socket, "~> 0.3"},
       {:ex_doc, "~> 0.14", only: :dev, runtime: false}
+    ]
+  end
+
+  defp package do
+    [ files: ~w(lib mix.exs README.md LICENSE),
+      maintainers: ["Takuji Shimokawa"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/takuji/typetalk-elixir"}
     ]
   end
 end
