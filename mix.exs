0f5247816354f5751697997f973b5fac5e20ae1f
Takuji Shimokawa <takuji.shimokawa@gmail.com>
2017-04-15 17:47:38 +0900

    change the version to 0.1.1

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Typetalk.Mixfile do
 
   def project do
     [app: :typetalk,
-     version: "0.1.0",
+     version: "0.1.1",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
